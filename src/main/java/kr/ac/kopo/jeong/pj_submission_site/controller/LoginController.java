eed01f0c38eba7f420d01d5e7a54e3ceacd212c1
a71143055 <a71143055@gmail.com>
2025-09-24 20:32:29 +0900

    PJ02

--- src/main/java/kr/ac/kopo/jeong/pj_submission_site/controller/LoginController.java
+++ src/main/java/kr/ac/kopo/jeong/pj_submission_site/controller/LoginController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
     @GetMapping("/login")
     public String showLoginPage() {
         return "login"; // login.html 렌더링
