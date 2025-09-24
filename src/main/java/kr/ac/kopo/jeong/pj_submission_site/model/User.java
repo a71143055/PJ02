7273409c31181df1117521cec1069f39cde280ca
a71143055 <a71143055@gmail.com>
2025-09-24 21:03:32 +0900

    PJ02

--- src/main/java/kr/ac/kopo/jeong/pj_submission_site/model/User.java
+++ src/main/java/kr/ac/kopo/jeong/pj_submission_site/model/User.java
@@ -3,18 +3,23 @@ package kr.ac.kopo.jeong.pj_submission_site.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "VARCHAR(36)")
     private String id;
+
     private String username;
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 }
-
--- src/main/java/kr/ac/kopo/jeong/pj_submission_site/service/CustomUserDetailsService.java
+++ src/main/java/kr/ac/kopo/jeong/pj_submission_site/service/CustomUserDetailsService.java
@@ -4,9 +4,7 @@ import kr.ac.kopo.jeong.pj_submission_site.model.User;
 import kr.ac.kopo.jeong.pj_submission_site.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
 
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
