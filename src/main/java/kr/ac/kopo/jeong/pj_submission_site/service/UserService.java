5dfb0a5bdabe92adeff31a8cb8bd05c5bc094b2e
a71143055 <a71143055@gmail.com>
2025-09-25 11:53:43 +0900

    PJ02

--- src/main/java/kr/ac/kopo/jeong/pj_submission_site/service/UserService.java
+++ src/main/java/kr/ac/kopo/jeong/pj_submission_site/service/UserService.java
@@ -1,6 +1,5 @@
 package kr.ac.kopo.jeong.pj_submission_site.service;
 
-import jakarta.transaction.Transactional;
 import kr.ac.kopo.jeong.pj_submission_site.model.Role;
 import kr.ac.kopo.jeong.pj_submission_site.model.User;
 import kr.ac.kopo.jeong.pj_submission_site.repository.UserRepository;
@@ -25,11 +24,8 @@ public class UserService {
         userRepository.save(user);
     }
 
-    @Transactional
     public void deleteUserByUsername(String username) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-        userRepository.delete(user);
+        userRepository.deleteByUsername(username);
     }
 
 
